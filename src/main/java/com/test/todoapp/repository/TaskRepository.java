2408e348ef23dc0bb6b45205baad4a7787f02123
KenyTuan <vothanhtuan098@gmail.com>
2025-02-27 06:31:05 +0700

    update-repository

--- src/main/java/com/test/todoapp/repository/TaskRepository.java
+++ src/main/java/com/test/todoapp/repository/TaskRepository.java
@@ -6,12 +6,12 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
-@EnableMongoRepositories
+@Repository
 public interface TaskRepository extends MongoRepository<Task, String> {
 
     @Query("{'objStatus': 'ACTIVE'}")
--- src/main/java/com/test/todoapp/repository/UserRepository.java
+++ src/main/java/com/test/todoapp/repository/UserRepository.java
@@ -3,11 +3,11 @@ package com.test.todoapp.repository;
 import com.test.todoapp.model.entity.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@EnableMongoRepositories
+@Repository
 public interface UserRepository extends MongoRepository<User, String> {
 
     @Query("{'email': ?0, 'objStatus': 'ACTIVE'}")
